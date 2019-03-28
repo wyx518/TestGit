cb3318bc58451e94006fc5d5d5b694c787d4df22
wyx518 <15808006814@163.com>
2019-03-28 17:13:05 +0800

    first ccommit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    private static final int PLAYER_AMOUNT = 1;
+    public static void main(String[] args)//DAWRQWASD
+    {
+        System.out.println("helloworld!");
+        System.out.println(PLAYER_AMOUNT);
+    }
+}
